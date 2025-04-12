55cae2a66896e3f241cd3c640d4b14c618b655ab
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-04-12 17:06:59 +0100

    create server

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Primary file for the API
+ * 
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all request with a string
+const server = http.createServer(function(req, res) {
+    
+    // get the url and parse it
+
+    // get the path
+
+
+    // send the response
+
+    
+    // log the request path
+
+    res.end('Hello, World\n');
+});
+
+// start the server and have it listen on port 3000
+server.listen(3000, function() {
+    console.log("The server is listening on port 3000");
+})
